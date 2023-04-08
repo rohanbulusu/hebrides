3dd3577be2c74af57f848d58843423c2267ecaff
Rohan <Rohan@MacBook-Pro.local>
2023-04-07 18:16:38 -0700

    added Matrix implementation

--- hebrides/src/linal.rs
+++ hebrides/src/linal.rs
@@ -4,7 +4,7 @@
 //! support a wide array of operations in finite-dimensional space and
 //! form the basis of the linear algebra system for `hebrides`.
 
-use std::ops::{Add, Sub, Mul};
+use std::ops::{Add, Sub, Mul, Div};
 
 /// Representation of finite-dimensional vectors
 #[derive(Debug)]
@@ -15,7 +15,8 @@ pub struct Vector<T> {
 
 impl<T> Vector<T> where T: Clone {
 
-	fn new(components: &[T]) -> Vector<T> {
+	/// Constructs a `Vector` from a slice
+	pub fn new(components: &[T]) -> Vector<T> {
 		Vector {
 			components: components.to_vec(),
 			dim: components.len()
@@ -77,6 +78,67 @@ impl<T> Mul<Self> for Vector<T> where T: Copy + Mul<Output=T> + std::iter::Sum {
 		pairs.map(|pair| *pair.0 * *pair.1).sum()
 	}
 }
+
+impl<T> Div<T> for Vector<T> where T: Copy + Div<Output=T> {
+	type Output = Self;
+	fn div(self, other: T) -> Self {
+		let mut dividend = Vec::with_capacity(self.dim);
+		for component in self.components {
+			dividend.push(component / other);
+		}
+		Vector::new(dividend.as_slice())
+	}
+}
+
+fn all_rows_have_equal_length<'a, T>(array: &'a[&'a[T]]) -> bool {
+	let first_row_length = array[0].len();
+	for row in array {
+		if row.len() != first_row_length {
+			return false;
+		}
+	}
+	true
+}
+
+
+/// Container for the dimensions of a `Matrix`
+struct MatrixDimensions {
+	rows: usize,
+	cols: usize
+}
+
+impl MatrixDimensions {
+
+	fn new(num_rows: usize, num_cols: usize) -> MatrixDimensions {
+		Self {
+			rows: num_rows,
+			cols: num_cols
+		}
+	}
+
+}
+
+/// Representation of finite-dimensional matrices
+pub struct Matrix<'a, T> {
+	rows: &'a[&'a[T]],
+	dims: MatrixDimensions
+}
+
+impl<'a, T> Matrix<'a, T> where T: Clone {
+
+	/// Constructs a `Matrix` from nested slices
+	pub fn new(components: &'a [&'a [T]]) -> Matrix<'a, T> {
+		if !all_rows_have_equal_length(components) {
+			panic!("Expected all rows of Matrix to have equal length")
+		}
+		Self {
+			rows: components,
+			dims: MatrixDimensions::new(components.len(), components[0].len())		}
+	}
+
+}
+
+
 
 
 #[cfg(test)]
@@ -140,17 +202,18 @@ mod test {
 			use super::*;
 
 			#[test]
-			fn pos_elements() {
+			fn normal() {
 				let a = Vector::new(&[1, 2, 3]);
 				let b = Vector::new(&[4, 5, 6]);
 				assert_eq!(a + b, Vector::new(&[5, 7, 9]))
 			}
 
 			#[test]
-			fn neg_elements() {
-				let a = Vector::new(&[1, 2, 3]);
-				let b = Vector::new(&[-2, -3, -4]);
-				assert_eq!(a + b, Vector::new(&[-1, -1, -1]))
+			#[should_panic]
+			fn differing_dims() {
+				let a = Vector::new(&[1]);
+				let b = Vector::new(&[1, 2]);
+				let _ = a + b;
 			}
 
 		}
@@ -160,17 +223,18 @@ mod test {
 			use super::*;
 
 			#[test]
-			fn pos_elements() {
+			fn normal() {
 				let a = Vector::new(&[1, 2, 3]);
 				let b = Vector::new(&[3, 4, 5]);
 				assert_eq!(a - b, Vector::new(&[-2, -2, -2]))
 			}
 
 			#[test]
-			fn neg_elements() {
-				let a = Vector::new(&[1, 2, 3]);
-				let b = Vector::new(&[-1, -2, -3]);
-				assert_eq!(a - b, Vector::new(&[2, 4, 6]))
+			#[should_panic]
+			fn differing_dims() {
+				let a = Vector::new(&[1]);
+				let b = Vector::new(&[1, 2]);
+				let _ = a - b;
 			}
 
 		}
@@ -180,17 +244,10 @@ mod test {
 			use super::*;
 
 			#[test]
-			fn pos_elements() {
+			fn normal() {
 				let a = Vector::new(&[1, 2, 3]);
 				let b = Vector::new(&[1, 2, 3]);
 				assert_eq!(a * b, 14)
-			}
-
-			#[test]
-			fn neg_elements() {
-				let a = Vector::new(&[1, 2, 3]);
-				let b = Vector::new(&[-1, -2, -3]);
-				assert_eq!(a * b, -14)
 			}
 
 			#[test]
@@ -198,6 +255,72 @@ mod test {
 				let a = Vector::new(&[1, 0]);
 				let b = Vector::new(&[0, 1]);
 				assert_eq!(a * b, 0);
+			}
+
+			#[test]
+			#[should_panic]
+			fn differing_dims() {
+				let a = Vector::new(&[1]);
+				let b = Vector::new(&[1, 2]);
+				let _ = a * b;
+			}
+
+		}
+
+		mod division {
+
+			use super::*;
+
+			#[test]
+			fn normal() {
+				let a = Vector::new(&[2, 4, 6]);
+				assert_eq!(a / 2, Vector::new(&[1, 2, 3]))
+			}
+
+			#[test]
+			#[should_panic]
+			fn zero_divisor() {
+				let b = Vector::new(&[2, 3, 4]);
+				let _ = b / 0;
+			}
+
+		}
+
+	}
+
+	mod matrix {
+
+		use super::*;
+
+		mod dimensionality {
+
+			use super::*;
+
+			#[test]
+			#[should_panic]
+			fn incompatible_array() {
+				let _ = Matrix::new(&[
+					&[1, 2, 3],
+					&[1, 2]
+				]);
+			}
+
+			#[test]
+			fn num_rows() {
+				let m = Matrix::new(&[
+					&[1, 2, 3],
+					&[1, 2, 3]
+				]);
+				assert_eq!(m.dims.rows, 2)
+			}
+
+			#[test]
+			fn num_cols() {
+				let m = Matrix::new(&[
+					&[1, 2, 3],
+					&[1, 2, 3]
+				]);
+				assert_eq!(m.dims.cols, 3)
 			}
 
 		}
